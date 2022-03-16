d3854e33ce6cc713f83e3dc7f582f0fff367ffbe
uday <uday@mobifyi.com>
2022-03-16 06:54:21 +0530

    first commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Uday on 16/03/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
